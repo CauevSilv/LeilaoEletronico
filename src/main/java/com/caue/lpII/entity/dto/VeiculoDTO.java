dcc9ff2a64c94c704bbc0906fdd2af95ae53fb75
CauevSilv <caue.silva10@fatec.sp.gov.br>
2024-11-13 22:08:04 -0300

    :sparkles: feat: VeiculoDTO object

---
+++ src/main/java/com/caue/lpII/entity/dto/VeiculoDTO.java
@@ -0,0 +1,15 @@
+package com.caue.lpII.entity.dto;
+
+import com.caue.lpII.entity.TiposVeiculos;
+import lombok.Data;
+
+@Data
+public class VeiculoDTO {
+    private Long id;
+    private String descricao;
+    private Double valorInicial;
+    private boolean vendido;
+    private String modelo;
+    private String marca;
+    private TiposVeiculos tipo;
+}
